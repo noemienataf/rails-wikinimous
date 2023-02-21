bc0571124bac15aa6188cac1cc68eac0b1801433
Noemie Nataf <noemienataf@gmail.com>
2023-02-21 17:01:41 +0100

    exo3

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root to: "articles#index"
+  resources :articles
   # root "articles#index"
 end
